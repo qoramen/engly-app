21277637fd8e188f96133a6b542a93c99dc0bd43
qoramen <tojiboyevmurodbbek@gmail.com>
2025-07-31 16:31:21 +0500

    backend connect

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,33 @@
 import { Routes, Route } from 'react-router-dom';
 import { Footer, Navbar, Main, Whatwedo, Test, Contact } from '../';
 import { Box } from '@mui/material';
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://192.168.88.225:5000/api/readings")
+      .then((res) => {
+        setTasks(res.data[0]);
+        console.log(res.data[0]);
+      })
+      .catch((err) => {
+        console.error("Xatolik:", err);
+      });
+  }, []);
+
+
+  
   return (
     <Box>
       <Navbar />
       <Main />
-      <Whatwedo/>
-      <Test/>
-      <Contact/>
+      <Whatwedo />
+      <Test />
+      <Contact />
       {/* <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/tests' element={<Tests />} />
